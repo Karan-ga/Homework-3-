607343860d91e4863723fc84c9ede6355ed7268d
Karan Gangurde <karan.g98@gmail.com>
2021-03-24 16:33:00 +0000

    All arrays used to provide characters for password generation added to Javascript code

--- homework/Develop/script.js
+++ homework/Develop/script.js
@@ -1,6 +1,65 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lowercaseArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var uppercaseArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var numbersArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialcharArray = ["!", "@", "£", "%", "^", "&", "*", "+", ">", "<", "?"];
+
 // Declare a new function on line 3
 function generatePassword() {
   // Create a variable to store our generated password
